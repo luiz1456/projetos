d2fbc49f31c8bec3af299388ee61679968f7ee3e
luiz1456 <luizrodrigolimadesouza@gmail.com>
2024-01-02 21:49:25 -0300

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,43 @@ import Card from './Components/Card/Card'
 import ListaProjetos from './Dados/Dados'
 
 function App() {
-  let [listaFiltrada, setlistaFiltrada] = useState(ListaProjetos)
+  const [listaFiltrada, setlistaFiltrada] = useState(ListaProjetos)
+  const [botaoAtivo, setBotaoAtivo] = useState()
+
 
   const handleClick = (tecnologia) => {
-
+    setBotaoAtivo(tecnologia)
     setlistaFiltrada(
       tecnologia ? ListaProjetos.filter((projeto) =>
-      projeto.tecnologia.includes(tecnologia)) : ListaProjetos
-      )
+        projeto.tecnologia.includes(tecnologia)) : ListaProjetos
+    )
   }
 
   return (
     <main>
       <h1 className='titulo'>Meus projetos</h1>
-      <div className="container-botoes">
-        <button className='botoes' onClick={() => handleClick('html')}>Html e CSS</button>
-        <button className='botoes' onClick={() => handleClick('javascript')}>JavaScript</button>
-        <button className='botoes' onClick={() => handleClick('react')}>React</button>
-        <button className='botoes' onClick={() => handleClick()}>Todos</button>
-      </div>
-      <div className="container-cards">
-        {listaFiltrada.map((projeto, index) => {
-          return (
-            <Card key={index}
-              nome={projeto.nome}
-              descricao={projeto.descricao}
-              imagem={projeto.imagem}
-              link={projeto.link}
-              tecnologias={projeto.tecnologia}
-            />
-          )
-        })}
-      </div>
+      <section className="container-botoes">
+        <button className={`botoes ${botaoAtivo === 'html' ? 'botao-ativo' : ''}`} onClick={() => handleClick('html')}>Html e CSS</button>
+        <button className={`botoes ${botaoAtivo === 'javascript' ? 'botao-ativo' : ''}`} onClick={() => handleClick('javascript')}>JavaScript</button>
+        <button className={`botoes ${botaoAtivo === 'react' ? 'botao-ativo' : ''}`} onClick={() => handleClick('react')}>React</button>
+        <button className={`botoes ${!botaoAtivo ? 'botao-ativo' : ''}`} onClick={() => handleClick()}>Todos</button>
+      </section>
+      <section className="section-cards">
+        <div className="container-cards">
+          {listaFiltrada.map((projeto, index) => {
+            return (
+              <Card key={index}
+                nome={projeto.nome}
+                descricao={projeto.descricao}
+                imagem={projeto.imagem}
+                link={projeto.link}
+                tecnologias={projeto.tecnologia}
+                repositorio={projeto.repositorio}
+              />
+            )
+          })}
+        </div>
+      </section>
     </main>
   )
 }
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './styled.css'
 
 const renderTecnologia = (tecnologia) => {
@@ -14,39 +15,33 @@ const renderTecnologia = (tecnologia) => {
       return (
         <img src="assets/js.png" alt="javascript" title='JavaScript' />
       )
-      case 'react':
-        return (
-          <img className='logo-react' src="assets/react.svg" alt="react" title='React' />
-        )
+    case 'react':
+      return (
+        <img className='logo-react' src="assets/react.svg" alt="react" title='React' />
+      )
   }
 }
 
-// const formatarDescricao = (descricao) => {
-//   const descricaoFormatada = descricao.substring(0,87) +'...'
-//   // if(descricaoFormatada.length > 100)
-//   return(descricaoFormatada)
-// }
+
 
 export default function Card(props) {
   return (
     <div className='card'>
       <h1 className='nome-projeto'>{props.nome}</h1>
       <p className='descricao'>{props.descricao}</p>
-      <img className='imagem-projeto' src={props.imagem} alt={props.nome} />
+      <div className="container-img-projeto"><img className='imagem-projeto' src={props.imagem} alt={props.nome} /></div>
       <div className="tecnologias">
-
-        {props.tecnologias.map((tecnologia,index) => {
+        {props.tecnologias.map((tecnologia, index) => {
           return (
             <div className='container-img-tecnologia' key={index}>
               {renderTecnologia(tecnologia)}
             </div>
           )
         })}
-
       </div>
       <div className="container-links-projeto">
         <a className='link-projeto' href={props.link} target='_blank'>ver projeto</a>
-        <a className='link-repositorio' href="#" target='_blank'><img src="assets/github.png" alt="" /></a>
+        <a className='link-repositorio' href={props.repositorio} target='_blank'><img src="assets/github.png" alt="" /></a>
       </div>
     </div>
   )
