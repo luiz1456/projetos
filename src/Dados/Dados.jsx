e8bbf76fbadd2a8adf6200575ba98e43a860194e
luiz1456 <luizrodrigolimadesouza@gmail.com>
2024-01-04 15:00:23 -0300

    atualizaçãoes

--- src/Dados/Dados.jsx
+++ src/Dados/Dados.jsx
@@ -1,14 +1,91 @@
 const ListaProjetos = [
-    { nome: 'M agência', descricao: 'Um site de uma agência feito durante os estudos na plataforma da DEVMEDIA,feito com o intuito de praticar o uso de evento onClick.', imagem: 'assets/magencia.png', link: 'https://luiz1456.github.io/Magencia/', tecnologia: ['react'], repositorio: 'https://github.com/luiz1456/Magencia.git' },
-    { nome: 'sorveteria', descricao: 'Um site para uma sorveteria feito ultilizando React durante os estudos na plataforma da DEVMEDIA,com intuito de praticar a ultilização da biblioteca React Router.', imagem: 'assets/sorveteria.png', link: 'https://luiz1456.github.io/sorveteria_react/', tecnologia: ['react'], repositorio: 'https://github.com/luiz1456/sorveteria_react.git' },
-    { nome: 'óticas vida', descricao: 'Um site para uma loja de óculos feito ultilizando React durante os estudos na plataforma da DEVMEDIA,com intuito de praticar componentização.', imagem: 'assets/oculos.png', link: 'https://luiz1456.github.io/otica/', tecnologia: ['react'], repositorio: 'https://github.com/luiz1456/otica.git' },
-    { nome: 'Boloco de notas', descricao: 'Uma aplicação de bloco de notas que salva notas no local storage feito ultilizando HTML,CSS e JavaScript.', imagem: 'assets/bloco-notas.png', link: 'https://luiz1456.github.io/notas/listanotas', tecnologia: ['html', 'css', 'javascript'], repositorio: 'https://github.com/luiz1456/notas.git' },
-    { nome: 'Adivinhar o numero', descricao: 'Um joguinho de adivinhar o número,feito no começo dos estudos de JavaScript', imagem: 'assets/jogo-adivinha.png', link: 'https://luiz1456.github.io/jogo/jogo de divinha/', tecnologia: ['html', 'css', 'javascript'], repositorio: 'https://github.com/luiz1456/jogo.git' },
-    { nome: 'Agência de viagens', descricao: 'Um site de uma agência de viagens feito durante os estudos na plataforma da DEVMEDIA,feito com intuito de praticar o uso de formulários.', imagem: 'assets/agencia-viagens.png', link: 'https://luiz1456.github.io/agencia/agencia-de-viagens/', tecnologia:['html', 'css'], repositorio: 'https://github.com/luiz1456/agencia.git' },
-    { nome: 'CM Life', descricao: 'Um site para uma clínica feito durante os estudos na plataforma da DEVMEDIA com o intuito de praticar a costrução de sites responsivos.', imagem: 'assets/clinica.png', link: 'https://luiz1456.github.io/clinica/projeto-clinica/', tecnologia: ['html', 'css'], repositorio: 'https://github.com/luiz1456/clinica.git' },
-    { nome: 'Pousada Secreta', descricao: 'Um site de uma pousada,feito durante os estudos na plataforma da DEVMEDIA.', imagem: 'assets/pousada-secreta.png', link: 'https://luiz1456.github.io/pousada-secreta/pousada-secreta/', tecnologia: ['html', 'css'], repositorio: 'https://github.com/luiz1456/pousada-secreta.git' },
-    { nome: 'AlfaTech', descricao: 'Um site para uma empresa de hospedagem de sites feito durante os estudos na plataforma da DEVMEDIA ultilizando as tecnologias HTML e CSS.', imagem: 'assets/site-hospedagem.png', link: 'https://luiz1456.github.io/hospedagem/hospedagem/', tecnologia: ['html', 'css'], repositorio: 'https://github.com/luiz1456/hospedagem.git' },
-    { nome: 'coffee house', descricao: 'Um site para uma cafeteria feito durante os estudos na plataforma da DEVMEDIA ultilizando as tecnologias HTML e CSS.', imagem: 'assets/cafeteria.png', link: 'https://luiz1456.github.io/cafeteria/cafeteria/', tecnologia: ['html', 'css'], repositorio: 'https://github.com/luiz1456/cafeteria.git' }
+    {
+        nome: 'M. agência',
+        descricao: 'Um site de uma agência feito durante os estudos na plataforma da DEVMEDIA,feito com o intuito de praticar o uso de evento onClick.',
+        imagem: 'assets/magencia.png',
+        link: 'https://luiz1456.github.io/Magencia/',
+        tecnologia: ['react'],
+        repositorio: 'https://github.com/luiz1456/Magencia.git'
+    },
+    {
+        nome: 'sorveteria',
+        descricao: 'Um site para uma sorveteria feito ultilizando React durante os estudos na plataforma da DEVMEDIA,com intuito de praticar a ultilização da biblioteca React Router.',
+        imagem: 'assets/sorveteria.png',
+        link: 'https://luiz1456.github.io/sorveteria_react/',
+        tecnologia: ['react'],
+        repositorio: 'https://github.com/luiz1456/sorveteria_react.git'
+    },
+    {
+        nome: 'óticas vida',
+        descricao: 'Um site para uma loja de óculos feito ultilizando React durante os estudos na plataforma da DEVMEDIA,com intuito de praticar componentização.',
+        imagem: 'assets/oculos.png',
+        link: 'https://luiz1456.github.io/otica/',
+        tecnologia: ['react'],
+        repositorio: 'https://github.com/luiz1456/otica.git'
+    },
+    {
+        nome: 'Boloco de notas',
+        descricao: 'Uma aplicação de bloco de notas que salva notas no local storage feito ultilizando HTML,CSS e JavaScript.',
+        imagem: 'assets/bloco-notas.png',
+        link: 'https://luiz1456.github.io/notas/listanotas',
+        tecnologia: ['html', 'css', 'javascript'],
+        repositorio: 'https://github.com/luiz1456/notas.git'
+    },
+    {
+        nome: 'Adivinhar o numero',
+        descricao: 'Um joguinho de adivinhar o número,feito no começo dos estudos de JavaScript',
+        imagem: 'assets/jogo-adivinha.png', link: 'https://luiz1456.github.io/jogo/jogo de adivinha/',
+        tecnologia: ['html', 'css', 'javascript'],
+        repositorio: 'https://github.com/luiz1456/jogo.git'
+    },
+    {
+        nome: 'Agência de viagens',
+        descricao: 'Um site de uma agência de viagens feito durante os estudos na plataforma da DEVMEDIA,feito com intuito de praticar o uso de formulários.',
+        imagem: 'assets/agencia-viagens.png',
+        link: 'https://luiz1456.github.io/agencia/agencia-de-viagens/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/agencia.git'
+    },
+    {
+        nome: 'CM Life',
+        descricao: 'Um site para uma clínica feito durante os estudos na plataforma da DEVMEDIA com o intuito de praticar a costrução de sites responsivos.',
+        imagem: 'assets/clinica.png',
+        link: 'https://luiz1456.github.io/clinica/projeto-clinica/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/clinica.git'
+    },
+    {
+        nome: 'Pousada Secreta',
+        descricao: 'Um site de uma pousada,feito durante os estudos na plataforma da DEVMEDIA.',
+        imagem: 'assets/pousada-secreta.png',
+        link: 'https://luiz1456.github.io/pousada-secreta/pousada-secreta/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/pousada-secreta.git'
+    },
+    {
+        nome: 'AlfaTech',
+        descricao: 'Um site para uma empresa de hospedagem de sites feito durante os estudos na plataforma da DEVMEDIA ultilizando as tecnologias HTML e CSS.',
+        imagem: 'assets/site-hospedagem.png',
+        link: 'https://luiz1456.github.io/hospedagem/hospedagem/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/hospedagem.git'
+    },
+    {
+        nome: 'coffee house',
+        descricao: 'Um site para uma cafeteria feito durante os estudos na plataforma da DEVMEDIA ultilizando as tecnologias HTML e CSS.',
+        imagem: 'assets/cafeteria.png',
+        link: 'https://luiz1456.github.io/cafeteria/cafeteria/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/cafeteria.git'
+    },
+    {
+        nome: 'LinkBio Neymar',
+        descricao: 'Um LinkBio ultilizando um video com fundo,feito com o intuito de praticar pseudos elementos.',
+        imagem: 'assets/linkBio.png',
+        link: 'https://luiz1456.github.io/linkbioneymar/',
+        tecnologia: ['html', 'css'],
+        repositorio: 'https://github.com/luiz1456/linkbioneymar.git'
+    }
 ]
 
 export default ListaProjetos;
